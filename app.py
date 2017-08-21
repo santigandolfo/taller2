e811c8b90f998c4b5facaa747e4b0a9a96df7610
Gonzalo Petraglia <gonzalpetraglia@gmail.com>
2017-08-20 22:17:03 -0300

    Agregando soporte de coveralls

--- app.py
+++ app.py
@@ -7,5 +7,13 @@ application = Flask(__name__)
 def index():
     return AppController.index()
 
+@application.route("/api/post", methods=['GET'])
+def api_post():
+    data = []    
+    data.append( { "mensaje":"Buenas buenas, la app se comunica con el server."} )
+    return "Buenas buenas, la app se comunica con el server.\n"
+
+
+
 if __name__ == "__main__":
     application.run(debug=True, host='0.0.0.0')
