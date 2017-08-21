691ec077105061c66fdbb1b3009558efee522804
Gonzalo Petraglia <gonzalpetraglia@gmail.com>
2017-08-21 00:36:44 -0300

    Trying to cover more code with tests

--- tests/test_AppController.py
+++ tests/test_AppController.py
@@ -1,3 +1,7 @@
+import sys
+sys.path.append('..')
+from src.controllers import AppController
+import re
 def test_1():
   ret_value = "value"
   assert b"value" in ret_value
@@ -5,3 +9,9 @@ def test_1():
 def test_2():
   ret_value = 2
   assert 1 + 1 == ret_value
+
+#Check if it is a html header
+def test_dummy():
+
+  assert re.match("<h(\d)>[\s\w\d\s]*<\/h\1>",AppControler.index())
+  
