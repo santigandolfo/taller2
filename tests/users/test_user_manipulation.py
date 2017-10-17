7f410952156cd9a65a31d40506ae4a620096a0ed
Gonzalo Petraglia <gonzalpetraglia@gmail.com>
2017-10-17 00:04:08 -0300

    add more tests

--- tests/users/test_user_manipulation.py
+++ tests/users/test_user_manipulation.py
@@ -92,6 +92,107 @@ class TestBasic(BaseTestCase):
                 self.assertEqual(response.content_type,'application/json')
                 self.assertEqual(response.status_code,400)
 
+    def test_data_update_missing_token(self):
+        """ Test case for a failing data update because missing token"""
+        
+        with self.client:
+            auth_token = ''
+            with patch('requests.post') as mock_post:
+                mock_post.return_value = Mock()
+                mock_post.return_value.json.return_value = {'status':'success','message':'user_registered','auth_token':'fmsdakfkldskafl.fdsalfkdsa.fdsafsd', "user":{"username":"joe_smith"}}
+                mock_post.return_value.ok = True
+                mock_post.return_value.status_code = 201
+                response = self.client.post(
+                    '/users',
+                    data=json.dumps(dict(
+                        username='joe_smith',
+                        password='123456'
+                    )),
+                    content_type='application/json'
+                )
+                data = json.loads(response.data.decode())
+                auth_token = data['auth_token']
+            with patch('requests.put') as mock_put:
+                response = self.client.put(
+                    '/users',
+                    data=json.dumps(dict(
+                        data=1
+                    )),
+                    content_type='application/json'
+
+                    )
+
+                data = json.loads(response.data.decode())
+                self.assertEqual(data['status'],'fail')
+                self.assertEqual(data['message'],'missing_token')
+                self.assertEqual(response.content_type,'application/json')
+                self.assertEqual(response.status_code,401)
+
+
+    def test_data_update_with_expired_token(self):
+        """ Test case for a failing data update because of expired token"""
+        
+        with self.client:
+            auth_token = ''
+            with patch('requests.post') as mock_post:
+                mock_post.return_value = Mock()
+                mock_post.return_value.json.return_value = {'status':'success','message':'user_registered','auth_token':'fmsdakfkldskafl.fdsalfkdsa.fdsafsd', "user":{"username":"joe_smith"}}
+                mock_post.return_value.ok = True
+                mock_post.return_value.status_code = 201
+                response = self.client.post(
+                    '/users',
+                    data=json.dumps(dict(
+                        username='joe_smith',
+                        password='123456'
+                    )),
+                    content_type='application/json'
+                )
+                data = json.loads(response.data.decode())
+                auth_token = data['auth_token']
+            time.sleep(TOKEN_DURATION+1)
+            with patch('requests.put') as mock_put:
+                response = self.client.put(
+                    '/users',
+                    data=json.dumps(dict(
+                        data=1
+                    )),
+                    headers=dict(
+                        Authorization='Bearer '+auth_token
+                    ),
+                    content_type='application/json'
+
+                    )
+
+                data = json.loads(response.data.decode())
+                self.assertEqual(data['status'],'fail')
+                self.assertEqual(data['message'],'expired_token')
+                self.assertEqual(response.content_type,'application/json')
+                self.assertEqual(response.status_code,401)
+
+    def test_data_update_with_invalid_token(self):
+        """ Test case for a failing data update because of invalid token"""
+        
+        with self.client:
+            
+            with patch('requests.put') as mock_put:
+                response = self.client.put(
+                    '/users',
+                    data=json.dumps(dict(
+                        data=1
+                    )),
+                    headers=dict(
+                        Authorization='Bearer kfdslfdajsfds.jfjdsalkjfasdjk.fdsajfskd'
+                    ),
+                    content_type='application/json'
+
+                    )
+
+                data = json.loads(response.data.decode())
+                self.assertEqual(data['status'],'fail')
+                self.assertEqual(data['message'],'invalid_token')
+                self.assertEqual(response.content_type,'application/json')
+                self.assertEqual(response.status_code,401)
+
 
   
 if __name__ == '__main__':
