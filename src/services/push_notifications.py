547af906d9d68d47c9534eba99213cdbbbb0ef2b
Gonzalo L Petraglia <gonzalpetraglia@gmail.com>
2017-12-07 14:46:14 -0300

    Add some functionality to the backoffice

--- src/services/push_notifications.py
+++ src/services/push_notifications.py
@@ -5,13 +5,13 @@ from app import application
 
 
 FIREBASE_KEY = os.environ["FIREBASE_KEY"]
+push_service = FCMNotification(api_key=FIREBASE_KEY)
 
 
 def send_push_notifications(username, message_body, data_message):
     application.logger.info("Sending push notification to user: {}".format(username))
     user = User.get_user_by_username(username)
     try:
-        push_service = FCMNotification(api_key=FIREBASE_KEY)
         registration_id = user.push_token
         application.logger.info("Sending push notification to user registered with firebase id: {}".format(registration_id))
         result = push_service.notify_single_device(registration_id=registration_id,
@@ -22,3 +22,8 @@ def send_push_notifications(username, message_body, data_message):
         return result
     except:
         pass
+
+
+def send_message_to_multiple_users(title, message_body, token_lists):
+    push_service.notify_multiple_devices(message_body=message_body, registration_ids=token_lists,
+                                         message_title=title)
